a3244c2d0d9d74d367b892ab60eab42b55b5c18a
Aaron Williams <Aaron.Williams@Polymathic.Company>
2025-10-10 14:48:28 -0400

    Remove test elements.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,3 @@ export { default as LeContext } from "./components/le-context";
 export { default as LeEach } from "./components/le-each";
 export { default as LeElse } from "./components/le-else";
 export { default as LeIf } from "./components/le-if";
-export { default as TestElement } from "./components/test-component";
-export { default as TestAnotherElement } from "./components/test-another-component";
